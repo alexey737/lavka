a209281977af787950525fc8af0e4f588971c61d
Aleksey <lavka.alexe37@gmail.com>
2020-11-17 03:33:11 +0200

    lab04

--- lab04/5/src/main.c
+++ lab04/5/src/main.c
@@ -1,17 +1,18 @@
-
-int main(){
-     int x = 2;
+#include <stdio.h>
+int main(){     
+     int x;
      int y;
-     
-             if (x >= -3 && x < 1){
+     printf("Введите число: ");
+     scanf("%d", &x);
+             if (x >= -3 && x < -1){
                   y = -x - 1;
                   }
-             else if (x >= -1 && x <= 1){     
-                       y = -x + 1;
-                       }
-             else if (x > 1 && x <= 3){     
+             else if (x >= -1 && x < 1){     
                        y = x + 1;
+                       }
+             else if (x >= 1 && x <= 3){     
+                       y = x - 1;
              }
-
+             printf("y = %d", y);
 return 0;
 }             
